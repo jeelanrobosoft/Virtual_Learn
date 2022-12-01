e375d576aa1da6091322066527702f80529f8fa3
AK_Jeelan <jeelan.ak@robosoft.in>
2022-12-01 17:06:08 +0530

    14 commit

--- src/main/java/com/robosoft/VirtualLearn/AdminPanel/config/SwaggerConfig.java
+++ src/main/java/com/robosoft/VirtualLearn/AdminPanel/config/SwaggerConfig.java
@@ -5,21 +5,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKeys(){
+        return new ApiKey("JWT_TOKEN",AUTHORIZATION_HEADER,"header");
+    }
+
+    private List<SecurityContext> securityContexts(){
+        return Arrays.asList(SecurityContext.builder().securityReferences(sf()).build());
+    }
+
+    private List<SecurityReference> sf(){
+        AuthorizationScope scope = new AuthorizationScope("Global","accessEverything");
+        return Arrays.asList(new SecurityReference("JWT_TOKEN",new AuthorizationScope[] { scope }));
+    }
 
     @Bean
     public Docket api(){
 
         return new  Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(getInfo()).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+                .apiInfo(getInfo()).securityContexts(securityContexts()).securitySchemes(Arrays.asList(apiKeys())).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
     }
 
     private ApiInfo getInfo(){
--- src/main/java/com/robosoft/VirtualLearn/VirtualLearnApplication.java
+++ src/main/java/com/robosoft/VirtualLearn/VirtualLearnApplication.java
@@ -2,18 +2,25 @@ package com.robosoft.VirtualLearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
 @EnableScheduling
 @EnableWebMvc
-public class VirtualLearnApplication {
+public class VirtualLearnApplication extends WebMvcConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(VirtualLearnApplication.class, args);
 
 	}
 
 
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.defaultContentType(MediaType.APPLICATION_JSON);
+	}
 }
